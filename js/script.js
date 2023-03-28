0d65b199982d9b654541eb376969158afe34a3ef
118110103 JOAO VITOR DE MELO CAVALCANTE E SOUZA <joao.vitor.souza@lcc1-25.lcc.ufcg.edu.br>
2023-03-28 08:13:40 -0300

    HW

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert("hello world!");
+var mensagem = "Hello World!";
+var a = 2;
+var b = 3;
+
+if (b%2 == 1){
+	alert("ímpar");
+}
+else{
+	alert("Par");
+}
+
+
+
+alert(mensagem);
+//Teste Comentario
