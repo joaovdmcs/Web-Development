c335c385104763456c0fd5b55fefab2152baea04
João Vitor de Melo Cavalcante e Souza <joao.vitor.souza@ccc.ufcg.edu.br>
2023-05-04 17:38:05 -0300

    Retomada Web

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ else{
 
 alert(mensagem);
 //Teste Comentario
+
+i = 0;
+while (i<3){
+	alert(i);
+	i++;
+}
+
+
+for(var j = 0; j < 3; j++){
+	alert(j);
+}
+
+var numero = 1;
+var decimal = 4.6;
+var texto = "Oi";
+
+var lista = ["a","b","c"];
